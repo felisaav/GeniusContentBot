97df127d682eaa20575fa95788f2bb49ead286bd
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2024-01-01 13:10:43 -0300

    Update GeniusContentBot.py

--- GeniusContentBot.py
+++ GeniusContentBot.py
@@ -54,7 +54,7 @@ def main():
 		    )
 		    response_content = response["choices"][0]["message"]["content"]
 		    txt=st.write(response_content) #text_area("Content Proposal",response_content)
-		st.download_button('Download CSV', text_contents) 
+		#st.download_button('Download CSV', text_contents) 
 			
 if __name__ == '__main__':
 	main()
